8bd102c29c0406a88b08cd8467be5236f1a8876d
Ghabaei-Behzad <Ghabaei_Behzad@student.smc.edu>
2026-02-11 19:00:31 -0800

    Create BudgetTracker.js
    added

---
+++ src/BudgetTracker.js
@@ -0,0 +1,19 @@
+// Behzad Ghabaei
+// CS 81 - JavaScript
+// App.js - ContactForm.js
+// Assignment 11A - React Contact Form
+// Instructor Seno
+// Feb. 10, 2026
+
+import React from 'react';
+import BudgetTracker from './BudgetTracker';
+
+function App() {
+  return (
+    <div className="App">
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
